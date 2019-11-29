d28dbacde519d395fa917ef561e2d2c480293698
JY <yimji@oregonstate.edu>
2019-11-29 13:25:16 -0700

    debug test07

--- projects/FinalProject-Bugs/dominion/test07.c
+++ projects/FinalProject-Bugs/dominion/test07.c
@@ -15,7 +15,7 @@ int main(){
     int seed = 1000;
     int numPlayers = 2;
     struct gameState G, testG;
-    int choice1, choice3, choice3, handPos, bonus;
+    int choice1, choice2, choice3, handPos, bonus;
     int card = tribute;
     int k[10] = {baron, minion, ambassador, tribute, mine, adventurer, village, smithy, estate, council_room};
 
