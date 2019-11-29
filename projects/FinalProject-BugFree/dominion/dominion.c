f8d074cebfe878e4ed2d03f06e9b7932f52d680f
JY <yimji@oregonstate.edu>
2019-11-29 14:26:20 -0700

    bug 6 fixed

--- projects/FinalProject-BugFree/dominion/dominion.c
+++ projects/FinalProject-BugFree/dominion/dominion.c
@@ -759,7 +759,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         //Backup hand
 
         //Update Coins for Buy
-        updateCoins(currentPlayer, state, 5);
+        //updateCoins(currentPlayer, state, 5);
         x = 1;//Condition to loop on
         while( x == 1) {//Buy one card
             if (supplyCount(choice1, state) <= 0) {
@@ -770,12 +770,13 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
                     printf("Cards Left: %d\n", supplyCount(choice1, state));
                 }
             }
-            else if (state->coins < getCost(choice1)) {
+            else if (getCost(choice1) > 5) {
                 printf("That card is too expensive!\n");
-
-                if (DEBUG) {
-                    printf("Coins: %d < %d\n", state->coins, getCost(choice1));
-                }
+                printf("Please enter a new card choice to buy.\n");
+                scanf("%d", &choice1);
+                //if (DEBUG) {
+                //    printf("Coins: %d < %d\n", state->coins, getCost(choice1));
+                //}
             }
             else {
 
