f8e7f89570cd44dc69ed74409728ddb98928f341
sorgek <urmom@mom.com>
2019-11-29 21:16:25 -0800

    Reports/Tests for Bugs 8 & 9

---
+++ projects/FinalProject-Bugs/dominion/test09.c
@@ -0,0 +1,30 @@
+/*
+** Name: Emily Sorg
+** CS 362 Software Engineering II
+** Final Project
+** Description: This unit test that coins from playcard are updated correclty
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "test_helpers.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    //initialize gameState
+
+    struct gameState *state = newTestState();
+    int player = state->whoseTurn;
+    int player2 = state->whoseTurn + 1;
+
+    addCardToPlayersHand(state, smithy, 0, player2);
+    addCardToPlayersHand(state, smithy, 1, player2);
+    state->numActions = 0;
+
+    //Play baron card
+    cardEffect(tribute, 0, 0, 0, state, 1, 0);
+    safeAssert(state->numActions == 2, "When top two cards of opponents deck are smithies, tribute card adds 2 numActions");
+    return 0;
+}
---
+++ projects/FinalProject-Bugs/dominion/test_helpers.c
@@ -0,0 +1,74 @@
+#include "test_helpers.h"
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <malloc.h>
+
+struct gameState* newTestState(){
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    struct gameState *g = (struct gameState*)malloc(sizeof(struct gameState));
+    memset(g,0,sizeof(struct gameState));
+
+    initializeGame(2,k,1,g);
+
+    return g;
+}
+
+struct gameState* refreshGameState(struct gameState* g){
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    memset(g,0,sizeof(struct gameState));
+    initializeGame(2,k,1,g);
+
+    return g;
+}
+
+int addCardToPlayersHand(struct gameState* game, int cardToAdd, int cardPos, int player){
+    int currentCards = game->handCount[player];
+    if(currentCards < cardPos + 1){
+        int start;
+        if(currentCards == 0) start = 0;
+        else start = currentCards - 1;
+        for(int i = start; i <= cardPos; i++){
+            drawCard(player, game);
+        }
+    }
+    game->hand[player][cardPos] = cardToAdd;
+}
+
+
+int addSingleCardToHand(struct gameState* game, int cardToAdd, int cardPos){
+    addCardToPlayersHand(game, cardToAdd, cardPos, game->whoseTurn);
+}
+
+
+int addAllCards(struct gameState* game, int cardToAdd, int player){
+
+    int currentCards = game->handCount[player];
+
+    for(int i = 0; i <= currentCards; i++){
+        game->hand[player][i] = cardToAdd;
+    }
+
+}
+
+int safeAssert(int eval, char* testName){
+
+    if(eval){
+
+    } else {
+        printf("FAILED: %s \n", testName);
+    }
+    return 0;
+}
+
+int safeAssertForI(int eval, char* testName, int i){
+
+    if(eval){
+
+    } else {
+        printf("FAILED for x = %i: %s \n", i, testName);
+    }
+    return 0;
+}
