9ab7d8d0ab8f1284be9030e750370ffc12443514
siyuan_chen <chens5@oregonstate.edu>
2019-12-03 05:01:32 -0800

    test 01-03

---
+++ projects/FinalProject-BugFree/dominion/test01.c
@@ -0,0 +1,44 @@
+
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "interface.h"
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+	int seed = 1000;
+	int currentPlayer = 2;
+	int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, ambassador, tribute, baron, minion };
+	// declare the game state
+	struct gameState G;
+	
+	
+
+	printf("Bug 01\n");
+
+	memset(&G, 23, sizeof(struct gameState)); // set the game state
+	initializeGame(currentPlayer, k, seed, &G); // initialize a new game
+
+	// Get initial played card count 
+	int initialPlayedCardCount = G.playedCardCount;
+	// Add silver to player 0's hand
+	G.hand[0][4] = silver;
+	int coin_bonus;
+
+	// Call cardEffect function
+	cardEffect(mine, 4, gold, 0, &G, 0, &coin_bonus);
+
+	// verify that your asserts pass, for the bugs your asserts may fail
+	// playedCardCount should only go up by 1, which is the mine card being played
+	if (assert(initialPlayedCardCount + 1==G.playedCardCount)) {
+		printf("Passed\n");
+	}
+	else {
+		printf("Failed\n");
+	}
+
+	return 0;
+}
